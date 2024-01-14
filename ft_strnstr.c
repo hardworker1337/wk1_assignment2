1127147053279ff8104444e0fa139aca59ebe59f
hardworker1337 <nafiaboubaker@gmail.com>
2024-01-14 13:04:52 +0100

    ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,31 @@
+#include <stddef.h>
+
+static int	ft_cmpchr(const char *cs1, const char *cs2, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	while (cs2[i] && i < len)
+	{
+		if (cs1[i] != cs2[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	if (*little == '\0')
+		return ((char *)big);
+	while (big[i])
+	{
+		if (ft_cmpchr(&big[i], &little[0]) == 0)
+			return ((char *)&big[i]);
+		i++;
+	}
+	return (NULL);
+}
