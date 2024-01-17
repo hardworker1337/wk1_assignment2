49b0bae065333a63133ecbd4c832ac01b8751b95
hardworker1337 <nafiaboubaker@gmail.com>
2024-01-17 14:29:24 +0100

    ft_strjoin.c

---
+++ ft_strjoin.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bnafia <bnafia@student.42madrid.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/16 18:50:31 by bnafia            #+#    #+#             */
+/*   Updated: 2024/01/17 14:29:05 by nafia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+static int	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int	s1len;
+	int	s2len;
+	int	i;
+	int	j;
+
+	s1len = ft_strlen(s1);
+	s2len = ft_strlen(s2);
+	char	*tmp = (char *)malloc(sizeof(char) * (s1len + s2len) + 1);
+	if (!tmp)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		tmp[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		tmp[i + j] = s2[j];
+		j++;
+	}
+	tmp[i + j] = '\0';
+	return (tmp);
+}
